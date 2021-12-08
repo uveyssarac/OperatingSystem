591cc92ce85b3b10a1ce248390828e013965d1b1
uveyssarac <uveys.sarac@stu.fsm.edu.tr>
2021-12-09 02:08:06 +0300

    improve

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int count;
+	int count=0, count1=0;
 	int i,j,k;
 	char s[1000],s1[1000],*p;  
     int words=0;
@@ -36,13 +36,23 @@ int main(int argc, char *argv[]) {
 //   for( j =0 ; j < words; j++){
 //   	printf("%s\n",array[j]);
 //   }
-	printf("%s\n",s1);
+//	printf("%s\n",s1);
 	for (k = 0; s1[k] != '\0'; k++) {
         if (cizgi == s1[k])
             commands++;
     }
    
 //  printf("%d", commands);
-
+	if(commands >= 1){
+		char cArray[commands][50];
+		char * token1 = strtok(s1, "|");
+		while( token1 != NULL ) {
+      		printf("%s\n", token1 ); //printing each token
+      		strcpy(cArray[count], token1);
+      		token1 = strtok(NULL, "|");
+	  		count1++;
+		}
+		
+	}
 	return 0;
 }
